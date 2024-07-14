eea78d004221d088aa672029ffa2252992703d0d
HoangSon <son.lyhoang2014@gmail.com>
2024-07-14 09:07:15 +0700

    first commit

---
+++ src/main/java/com/sontung/eproject_springboot/EprojectSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.sontung.eproject_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EprojectSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EprojectSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sontung/eproject_springboot/EprojectSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sontung.eproject_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EprojectSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
