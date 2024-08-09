f9b36b594734d1da2cbd6fa0eeb92483da4b26f2
HoangSon <son.lyhoang2014@gmail.com>
2024-08-09 22:16:55 +0700

    Product detail, add to cart

--- src/main/java/com/sontung/eproject_springboot/controller/user/UserCartController.java
+++ src/main/java/com/sontung/eproject_springboot/controller/user/UserCartController.java
@@ -1,14 +1,35 @@
 package com.sontung.eproject_springboot.controller.user;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/cart")
+@CrossOrigin
 public class UserCartController {
     @GetMapping("/index")
     public String getCarts(){
         return "/user/cart/index";
+    }
+
+    @PostMapping("/add")
+    @ResponseBody
+    public ResponseEntity<Map<String, Object>> addToCart(@RequestParam("productId") String productId,
+                                                         @RequestParam("quantity") int quantity) {
+        Map<String, Object> response = new HashMap<>();
+//        try {
+//            // Logic để thêm sản phẩm vào giỏ hàng
+//            cartService.addToCart(productId, quantity);
+//            response.put("success", true);
+//        } catch (Exception e) {
+//            response.put("success", false);
+//        }
+//        return ResponseEntity.ok(response);
+        response.put("success", true);
+        return ResponseEntity.ok(response);
     }
 }
