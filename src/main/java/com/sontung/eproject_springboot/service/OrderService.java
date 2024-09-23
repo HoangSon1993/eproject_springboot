f3b6beb234bd09a43f437f6fdd71e11a1b4a7ab9
HoangSon1993 <133156560+HoangSon1993@users.noreply.github.com>
2024-09-23 14:41:31 +0700

    WIP: Page Admin_Order: Done search, sort, filter, pagination

--- src/main/java/com/sontung/eproject_springboot/service/OrderService.java
+++ src/main/java/com/sontung/eproject_springboot/service/OrderService.java
@@ -67,4 +67,6 @@ public interface OrderService {
 
     @Scheduled(fixedRate = 3600000) // Lặp lại mỗi giờ (3600000 ms = 1 giờ)
     void updatePendingOrders();
+
+    void confirmPaymentCOD(String orderId);
 }
